4595da42c0410e8fd6090630d33d85e275759df0
alimohammedkhan <93427945+alimohammedkhan@users.noreply.github.com>
2021-11-05 14:54:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let url = window.location.hostname; //Gets the hostname of the website
+
+switch (url){              //Loops the hostname to match with the company
+    case "www.puma.com";
+    alert("This company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.axa.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.hp.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.sodastream.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.ahava.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.sabra.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.pillsbury.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+    case "www.axa.com";
+    alert("This  company is complicit in human rights violation in Occupied Palestine");
+    break;
+}
+
